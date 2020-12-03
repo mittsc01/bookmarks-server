96a2d2c160b048f2b764438774cebcf01ce22319
Scott Mittman <sm2884@nau.edu>
2020-12-03 08:33:20 -0600

    added route handlers for /bookmarks and /bookmarks/:id

---
+++ src/store.js
@@ -0,0 +1,11 @@
+const bookmarks = [
+    {
+        title: "test",
+        url: "https://www.runningahead.com/maps",
+        description: 'map routes',
+        rating: 5,
+        id: "abcdef12"
+
+    }
+]
+module.exports = {bookmarks}
