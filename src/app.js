63204cec9b7dc95f17c45974c53246b55beb6d5d
Scott Mittman <sm2884@nau.edu>
2020-12-27 19:37:30 -0600

    change

--- src/app.js
+++ src/app.js
@@ -58,10 +58,11 @@ app.use('/api/bookmarks',bookmarksRouter)
 
 app.use(function errorHandler(error, req, res, next) {
     let response
+    console.error(error)
     if (NODE_ENV === 'production') {
         response = { error: { message: 'server error' } }
     } else {
-        console.error(error)
+        
         response = { message: error.message, error }
     }
     res.status(500).json(response)
